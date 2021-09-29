2a397de3628e39d63412ea5216a103067a4b1e72
Renan Bartalo <renan.bartalo@outlook.com>
2021-09-29 18:08:42 -0300

    changing routes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,9 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import ResultPage from "./ResultPage/ResultPage";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Home = () => {
   return (
     <div>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/result-page" component={ResultPage} />
-        </Switch>
-      </BrowserRouter>
+
     </div>
   );
 };
