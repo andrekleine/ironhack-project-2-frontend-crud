c9472691d825f46379597dd1a37fde3909b03553
andrekleine <andrekleine@gmail.com>
2021-09-30 11:44:15 -0300

    Finish Favorites area

--- src/components/Home/Favorites/Cards.js
+++ src/components/Home/Favorites/Cards.js
@@ -1,21 +1,109 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Cards = (props) => {
+const Cards = () => {
   return (
     <div>
       <svg width="320" height="350">
-        <rect y="0" rx="20" ry="20" width="320" height="100" fill="yellow" />
-        <rect y="50" rx="20" ry="20" width="320" height="100" fill="red" />
-        <rect y="100" rx="20" ry="20" width="320" height="100" fill="green" />
-        <rect y="150" rx="20" ry="20" width="320" height="100" fill="blue" />
-        <rect y="200" rx="20" ry="20" width="320" height="100" fill="black"> <text
-          x="230"
-          y="38"
-          fontSize="36px"
-          fontFamily="Tahoma"
-          fill="white">Restaurantes
-        </text></rect>
-        
+        <g>
+          {/* <Link to="#"> */}
+            <rect
+              y="0"
+              rx="20"
+              ry="20"
+              width="320"
+              height="100"
+              fill="yellow"
+            />
+            <text
+              x="30"
+              y="38"
+              fontSize="32px"
+              fontFamily="Tahoma"
+              fill="black"
+            >
+              Restaurantes
+            </text>
+          {/* </Link> */}
+        </g>
+
+        <g>
+          {/* <Link to="#"> */}
+            <rect y="50" rx="20" ry="20" width="320" height="100" fill="red" />
+            <text
+              x="30"
+              y="88"
+              fontSize="32px"
+              fontFamily="Tahoma"
+              fill="white"
+            >
+              Cafés
+            </text>
+          {/* </Link> */}
+        </g>
+        <g>
+          {/* <Link to="#"> */}
+            <rect
+              y="100"
+              rx="20"
+              ry="20"
+              width="320"
+              height="100"
+              fill="green"
+            />
+            <text
+              x="30"
+              y="138"
+              fontSize="32px"
+              fontFamily="Tahoma"
+              fill="white"
+            >
+              Livrarias
+            </text>
+          {/* </Link> */}
+        </g>
+        <g>
+          {/* <Link to="#"> */}
+            <rect
+              y="150"
+              rx="20"
+              ry="20"
+              width="320"
+              height="100"
+              fill="blue"
+            />
+            <text
+              x="30"
+              y="188"
+              fontSize="32px"
+              fontFamily="Tahoma"
+              fill="white"
+            >
+              Academias
+            </text>
+          {/* </Link> */}
+        </g>
+        <g>
+          {/* <Link to="#"> */}
+            <rect
+              y="200"
+              rx="20"
+              ry="20"
+              width="320"
+              height="100"
+              fill="black"
+            />
+            <text
+              x="30"
+              y="238"
+              fontSize="32px"
+              fontFamily="Tahoma"
+              fill="white"
+            >
+              Diversos
+            </text>
+          {/* </Link> */}
+        </g>
       </svg>
     </div>
   );
