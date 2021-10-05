d87df3acd2c30c9740c35219092a4fdb3b8afe62
andrekleine <andrekleine@gmail.com>
2021-10-05 11:39:07 -0300

    Make AddFav send objects w/ user/category/venue to ironhack API

--- src/components/AddFav/AddFav.js
+++ src/components/AddFav/AddFav.js
@@ -1,61 +1,87 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Modal from "react-bootstrap/Modal";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Dropdown } from "react-bootstrap/dist/react-bootstrap.min.js";
-
+import { useHistory } from "react-router";
+import axios from "axios";
 
 const AddFav = (props) => {
-    const [isOpen, setIsOpen] = React.useState(false);
-    const [category, setCategory] = React.useState(null)
-    
-console.log(props)
-console.log(category)
-
-    const showModal = () => {
-      setIsOpen(true);
-    };
+  const [isOpen, setIsOpen] = useState(false);
+  const [category, setCategory] = useState(null);
+  const [favObj, setFavObj] = useState({
+    user: props.user,
+    category: '',
+    venue: props.venue
+  });
   
-    const hideModal = () => {
-      setIsOpen(false);
-    };
+  console.log('AddFav, category: ',category);
 
-    const handleSelect=(e)=>{
-        console.log(Object.values(e.target));
-        let temp = Object.values(e.target)
-        setCategory(temp[1].children)
-      }
-const categories = ['Restaurantes & Cafés', 'Bares & Casas Noturnas', 'Pontos Turísticos', 'Serviços', 'Diversos']
-    
-    return(
-        <div>
-          <button onClick={showModal}>Add to your fav</button>
+  const showModal = () => {
+    setIsOpen(true);
+  };
+
+  const hideModal = () => {
+    setIsOpen(false);
+  };
+
+  const handleSelect = (e) => {
+    console.log(Object.values(e.target));
+    let temp = Object.values(e.target);
+    setCategory(temp[1].children);
+  };
+  
+  const categories = [
+    "Restaurantes & Cafés",
+    "Bares & Casas Noturnas",
+    "Pontos Turísticos",
+    "Serviços",
+    "Diversos",
+  ];
+
+  const history = useHistory();
+
+  const handleAdd = () => {
+    axios
+      .post("https://ironrest.herokuapp.com/meusFavoritos", favObj)
+      .then((response) => {
+        console.log(response);
+        history.push("/home");
+      })
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    setFavObj({...favObj, category: category});
+  }, [category]);
+
+  return (
+    <div>
+      <button onClick={showModal}>Add to your fav</button>
       <Modal show={isOpen} onHide={hideModal}>
         <Modal.Body>
-        <div className="dropdown">
-        <Dropdown onClick={(e) => handleSelect(e)} >
-          <Dropdown.Toggle variant="light" id="dropdown-basic">
-            Selecione a Categoria
-          </Dropdown.Toggle>
+          <div className="dropdown">
+            <Dropdown onClick={(e) => handleSelect(e)}>
+              <Dropdown.Toggle variant="light" id="dropdown-basic">
+                Selecione a Categoria
+              </Dropdown.Toggle>
 
-          <Dropdown.Menu>
-            {categories.map((category) => (
-              <Dropdown.Item eventkey={category} key={category}>
-                {category}
-              </Dropdown.Item>
-            ))}
-          </Dropdown.Menu>
-        </Dropdown>
-      </div>
-
+              <Dropdown.Menu>
+                {categories.map((category) => (
+                  <Dropdown.Item eventkey={category} key={category}>
+                    {category}
+                  </Dropdown.Item>
+                ))}
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
         </Modal.Body>
         <Modal.Footer>
           <button onClick={hideModal}>return</button>
-          <button>Add</button>
+          <button onClick={handleAdd}>Add</button>
         </Modal.Footer>
       </Modal>
-
-        </div>
-    )
-}
+    </div>
+  );
+};
 
 export default AddFav;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,7 +5,8 @@ import Favorites from "./Favorites/Favorites";
 import SearchBar from "./SearchBar/SearchBar";
 import Navbar from "../Navbar";
 
-const Home = () => {
+const Home = (props) => {
+  console.log('Home, user: ', props.user);  
 
   return (
     <div className="home">
