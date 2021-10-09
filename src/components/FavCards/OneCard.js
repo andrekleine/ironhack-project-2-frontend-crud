c6dba68d4d43fed0b7f283ec8e31da8842bc4cb6
Renan Bartalo <renan.bartalo@outlook.com>
2021-10-09 16:11:41 -0300

    fixing login and edit/del button color

--- src/components/FavCards/OneCard.js
+++ src/components/FavCards/OneCard.js
@@ -9,13 +9,14 @@ const OneCard = (props) => {
   const [showModalUpdate, setShowModalUpdate] = useState(false);
 
   const handleClick = () => {
-showModalUpdate ? setShowModal(false) : setShowModal(!showModal)
+    showModalUpdate ? setShowModal(false) : setShowModal(!showModal);
   };
 
   const handleClickUpdate = () => {
-    showModal ? setShowModalUpdate(false) : setShowModalUpdate(!showModalUpdate)
+    showModal
+      ? setShowModalUpdate(false)
+      : setShowModalUpdate(!showModalUpdate);
   };
-
 
   return (
     <div className="container-fluid">
@@ -38,14 +39,14 @@ showModalUpdate ? setShowModal(false) : setShowModal(!showModal)
             <div className="address">
               <Rating>{props.address}</Rating>
               <p>{props.comment}</p>
-            </div>            
+            </div>
             <div className="del">
               <span className="del" onClick={handleClick}>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
                   height="24"
-                  fill="red"
+                  fill="grey"
                   className="bi bi-trash-fill float-left"
                   viewBox="0 0 16 16"
                 >
@@ -59,11 +60,11 @@ showModalUpdate ? setShowModal(false) : setShowModal(!showModal)
                   xmlns="http://www.w3.org/2000/svg"
                   width="22"
                   height="22"
-                  fill="red"
+                  fill="grey"
                   className="bi bi-pencil-filll float-left"
                   viewBox="0 0 16 16"
                 >
-                  <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+                  <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
                 </svg>
               </span>
             </div>
@@ -74,7 +75,11 @@ showModalUpdate ? setShowModal(false) : setShowModal(!showModal)
         <DelFav onClick={handleClick} category={props.category} id={props.id} />
       ) : null}
       {showModalUpdate ? (
-        <UpdateFav onClick={handleClickUpdate} category={props.category} id={props.id} />
+        <UpdateFav
+          onClick={handleClickUpdate}
+          category={props.category}
+          id={props.id}
+        />
       ) : null}
     </div>
   );
